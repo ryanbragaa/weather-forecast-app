dd90d3756d2f5c645ed10328a9d5322f6ce0173c
RyanBraga <125956400+ryanbragaa@users.noreply.github.com>
2023-10-21 19:46:16 -0300

    scripts for left side information

--- script.js
+++ script.js
@@ -6,19 +6,21 @@ const key = "56aa5617ec6ff9c66775c95cd158c21a";
 
 function colocarDadosNaTela(dados) {
     
-    console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
     document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
     document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+    
+    document.querySelector(".cidadeDois").innerHTML = dados.name
+    document.querySelector(".tempDois").innerHTML = Math.floor(dados.main.temp) + "°C"
+
 }
 
 
 async function buscarCidade(cidade) {
     const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
-
-    
 
     colocarDadosNaTela(dados);
 }
