cd0abc723fd653fe432866e81b84d046829c9179
RyanBraga <125956400+ryanbragaa@users.noreply.github.com>
2023-10-21 16:23:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+const key = "56aa5617ec6ff9c66775c95cd158c21a";
+
+
+function colocarDadosNaTela(dados) {
+    
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    
+
+    colocarDadosNaTela(dados);
+}
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+
+    buscarCidade(cidade);
+}
